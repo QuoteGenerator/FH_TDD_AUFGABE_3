23ddb89b4abb85737837e21489d79cd11708000a
ResidentAleks123 <Aleksandartesinic720@gmail.com>
2026-06-02 10:59:37 +0200

    WITH AI HELP (CODE REFACTORED) (ADDED COMMENTS) - REFACTORING

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,69 +4,105 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+function increase(item, amount = 1) {
+  item.quality = Math.min(MAX_QUALITY, item.quality + amount);
+}
+
+function decrease(item, amount = 1) {
+  item.quality = Math.max(MIN_QUALITY, item.quality - amount);
+}
+
+function isAgedBrie(item) {
+  return item.name === 'Aged Brie';
+}
+
+function isBackstage(item) {
+  return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+}
+
+function isSulfuras(item) {
+  return item.name === 'Sulfuras, Hand of Ragnaros';
+}
+
+function isConjured(item) {
+  return item.name.startsWith('Conjured');
+}
+
+function updateNormal(item) {
+  const degrade = isConjured(item) ? 2 : 1;
+  decrease(item, degrade);
+
+  if (item.sell_in < 0) {
+    decrease(item, degrade);
+  }
+}
+
+function updateAgedBrie(item) {
+  increase(item);
+
+  if (item.sell_in < 0) {
+    increase(item);
+  }
+}
+
+function updateBackstage(item) {
+  increase(item);
+
+  if (item.sell_in < 11) increase(item);
+  if (item.sell_in < 6) increase(item);
+
+  if (item.sell_in < 0) {
+    item.quality = 0;
+  }
+}
+
+function updateItem(item) {
+  if (item.name === 'Sulfuras, Hand of Ragnaros') {
+    return;
+  }
+
+  const isAgedBrie = item.name === 'Aged Brie';
+  const isBackstage = item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  const isConjured = item.name.startsWith('Conjured');
+
+  // 1. NORMAL UPDATE (vor sell_in decrement!)
+  if (isAgedBrie) {
+    item.quality += 1;
+  } else if (isBackstage) {
+    item.quality += 1;
+
+    if (item.sell_in <= 10) item.quality += 1;
+    if (item.sell_in <= 5) item.quality += 1;
+  } else {
+    const degrade = isConjured ? 2 : 1;
+    item.quality -= degrade;
+  }
+
+  // 2. SELL_IN DECREMENT
+  item.sell_in -= 1;
+
+  // 3. AFTER SELL DATE LOGIC (IMPORTANT: jetzt sell_in schon reduziert!)
+  if (item.sell_in < 0) {
+    if (isAgedBrie) {
+      item.quality += 1;
+    } else if (isBackstage) {
+      item.quality = 0;
+    } else {
+      const degrade = isConjured ? 2 : 1;
+      item.quality -= degrade;
+    }
+  }
+
+  // 4. bounds clamp
+  if (item.quality < 0) item.quality = 0;
+  if (item.quality > 50) item.quality = 50;
+}
 
 function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          if (items[i].name.indexOf('Conjured') === 0) {
-              items[i].quality = items[i].quality - 2;
-          } else {
-              items[i].quality = items[i].quality - 1;
-          }
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-
-              if (items[i].name.indexOf('Conjured') === 0) {
-                  items[i].quality = items[i].quality - 2;
-              } else {
-                  items[i].quality = items[i].quality - 1;
-              }
-
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
+  for (let i = 0; i < items.length; i++) {
+    updateItem(items[i]);
   }
 }
